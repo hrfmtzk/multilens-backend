fa4fc1df694f2d3782f2376022b6920157b55b7e
Hirofumi Tsuzuki <hrfm.tsuzuki@gmail.com>
2021-12-15 00:12:52 +0900

    add snapshot test

--- tests/unit/test_multilens_stack.py
+++ tests/unit/test_multilens_stack.py
@@ -1,15 +1,41 @@
-import aws_cdk as core
+import json
+import os
+
+import aws_cdk as cdk
 import aws_cdk.assertions as assertions
+import pytest
+from pytest_snapshot.plugin import Snapshot
 
 from multilens.multilens_stack import MultilensStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in multilens/multilens_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = MultilensStack(app, "multilens")
-    template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+class TestMultilensStack:
+    @pytest.fixture
+    def environ(self) -> None:
+        return
+
+    @pytest.fixture
+    def app(self, environ) -> cdk.App:
+        return cdk.App()
+
+    @pytest.fixture
+    def env(self, environ) -> cdk.Environment:
+        return cdk.Environment(
+            account=os.getenv("CDK_DEFAULT_ACCOUNT"),
+            region=os.getenv("CDK_DEFAULT_REGION"),
+        )
+
+    def test_snapshot(
+        self, snapshot: Snapshot, app: cdk.App, env: cdk.Environment
+    ) -> None:
+        stack = MultilensStack(
+            app,
+            "Multilens",
+            env=env,
+        )
+        template_json = assertions.Template.from_stack(stack).to_json()
+
+        snapshot.assert_match(
+            json.dumps(template_json, indent=2),
+            "multilens_stack.json",
+        )
